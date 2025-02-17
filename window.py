37bf09f64e5e6ec65e40e684a525623f7cb10807
gainax2k1 <gainax2k1@gmail.com>
2025-02-16 20:42:51 -0800

    Point, Line, and Drawline added

--- window.py
+++ window.py
@@ -1,20 +1,19 @@
 from tkinter import Tk, BOTH, Canvas
+import time # part of optional pause in self.wait_for_close() loop
 
 class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-        self.root = Tk() 
+        self.root = Tk() # can change to self.__root for more privacy for root
         """
          Creates an instance of the Tk class, which initializes Tk and creates its associated Tcl interpreter.
          It also creates a toplevel window, known as the root window, which serves as the main window of the application.
          """
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
-        self.root.title("Maze Solver") #not sure if write method?
-        # ttk.Label(frm, text="Hello World!").grid(column=0, row=0)   #potential alternative title method?
-
+        self.root.title("Maze Solver") 
 
         bg_color = 'gray75' #arbitrary for now
 
@@ -23,7 +22,6 @@ class Window:
         self.canvas.pack() # Pack the Canvas widget
 
         self.running = False
-
 
     def redraw(self): #  Redraws all the graphics in the window.
         self.root.update_idletasks()
@@ -34,12 +32,42 @@ class Window:
 
         while self.running:
             self.redraw()
-        
+            time.sleep(0.01)  # Adds a slight pause to reduce CPU usage, optional
+
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x # x=0 left of screen  # public becauses not self.__x and self.__y
+        self.y = y # y=0 top of screen
+
+
+class Line:
+    def __init__(self, point_a, point_b):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def draw(self, canvas, fill_color): # Canvas?
+        x1 = self.point_a.x
+        y1 = self.point_a.y
+        x2 = self.point_b.x
+        y2 = self.point_b.y
+        canvas.create_line(x1, y1, x2, y2, fill = fill_color, width = 2)
+        
+
+
 def main():
     win = Window(800, 600)
+    point1 = Point(10, 100)
+    point2 = Point(50, 200)
+    line = Line(point1, point2)
+    win.draw_line(line, "red")
+
     win.wait_for_close()
 
 
